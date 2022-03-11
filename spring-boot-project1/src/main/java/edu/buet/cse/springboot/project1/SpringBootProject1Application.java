33ab8aa6069f128a2099763add0d63b90be3e193
Shamim Ahmed <shamim.buet.99@gmail.com>
2022-03-11 14:18:23 +1100

    Print spring bean names

--- spring-boot-project1/src/main/java/edu/buet/cse/springboot/project1/SpringBootProject1Application.java
+++ spring-boot-project1/src/main/java/edu/buet/cse/springboot/project1/SpringBootProject1Application.java
@@ -1,12 +1,21 @@
 package edu.buet.cse.springboot.project1;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootProject1Application {
+  private static final Logger logger = LogManager.getLogger(SpringBootProject1Application.class);
 
   public static void main(String[] args) {
-    SpringApplication.run(SpringBootProject1Application.class, args);
+    ApplicationContext appContext =
+        SpringApplication.run(SpringBootProject1Application.class, args);
+
+    for (String name : appContext.getBeanDefinitionNames()) {
+      logger.info("Bean name is: " + name);
+    }
   }
 }
