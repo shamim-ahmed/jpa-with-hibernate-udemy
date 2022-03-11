48a15cf57b5a3248b468595e58b1d0d7a519b700
Shamim Ahmed <shamim.buet.99@gmail.com>
2022-03-11 14:49:41 +1100

    Update test class

--- spring-boot-project1/src/test/java/edu/buet/cse/springbootproject1/SpringBootProject1ApplicationTests.java
+++ spring-boot-project1/src/test/java/edu/buet/cse/springbootproject1/SpringBootProject1ApplicationTests.java
@@ -3,7 +3,9 @@ package edu.buet.cse.springbootproject1;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import edu.buet.cse.springboot.project1.SpringBootProject1Application;
+
+@SpringBootTest(classes = SpringBootProject1Application.class)
 class SpringBootProject1ApplicationTests {
 
   @Test
