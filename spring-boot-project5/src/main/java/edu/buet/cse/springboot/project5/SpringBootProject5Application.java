48289c61e68e05bc512da57a7d91fd71663fe4fe
Shamim Ahmed <shamim.buet.99@gmail.com>
2022-03-12 18:42:06 +1100

    Add new project to demonstrate the usage of JPA NamedQuery

---
+++ spring-boot-project5/src/main/java/edu/buet/cse/springboot/project5/SpringBootProject5Application.java
@@ -0,0 +1,59 @@
+package edu.buet.cse.springboot.project5;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import edu.buet.cse.springboot.project5.entity.Person;
+import edu.buet.cse.springboot.project5.jpa.PersonJpaRepository;
+
+@SpringBootApplication
+@EntityScan(basePackages = {"edu.buet.cse.springboot.project5.entity"})
+public class SpringBootProject5Application implements CommandLineRunner {
+
+  @Autowired
+  private PersonJpaRepository personRepository;
+
+  private Logger logger = LogManager.getLogger(getClass());
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootProject5Application.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    List<Person> personList = personRepository.findAll();
+    logger.info("all Persons -> {}", personList);
+
+    Person person = personRepository.findById(1L);
+    logger.info("Person -> {}", person);
+
+    Person billGates = new Person();
+    billGates.setName("Bill Gates");
+    billGates.setLocation("USA");
+    billGates.setBirthDate(new Date());
+
+    billGates = personRepository.insert(billGates);
+    logger.info("New Person -> {}", billGates);
+
+    Calendar calendar = Calendar.getInstance();
+    calendar.set(Calendar.YEAR, 1955);
+    calendar.set(Calendar.MONTH, Calendar.OCTOBER);
+    calendar.set(Calendar.DAY_OF_MONTH, 28);
+    billGates.setBirthDate(calendar.getTime());
+
+    billGates = personRepository.update(billGates);
+    logger.info("Updated Person -> {}", billGates);
+
+    personRepository.deleteById(1L);
+    logger.info("Person with id 1 has been removed");
+  }
+}
---
+++ spring-boot-project5/src/main/java/edu/buet/cse/springboot/project5/entity/Person.java
@@ -0,0 +1,62 @@
+package edu.buet.cse.springboot.project5.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+
+@Entity
+@NamedQuery(name = "find_all_person", query = "SELECT p FROM Person p")
+public class Person {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+  private String location;
+
+  @Column(name = "birth_date")
+  private Date birthDate;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getLocation() {
+    return location;
+  }
+
+  public void setLocation(String location) {
+    this.location = location;
+  }
+
+  public Date getBirthDate() {
+    return birthDate;
+  }
+
+  public void setBirthDate(Date birthDate) {
+    this.birthDate = birthDate;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("[ id: %d, name: %s, location: %s, birthDate: %s ]", id, name, location,
+        birthDate);
+  }
+}
