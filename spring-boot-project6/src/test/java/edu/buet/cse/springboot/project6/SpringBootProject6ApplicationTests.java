adb4a8e3f36d6b3bac34cf225d5a4abdd5725fab
Shamim Ahmed <shamim.buet.99@gmail.com>
2022-03-12 23:17:57 +1100

    Add a unit test for findById method

--- spring-boot-project6/src/test/java/edu/buet/cse/springboot/project6/SpringBootProject6ApplicationTests.java
+++ spring-boot-project6/src/test/java/edu/buet/cse/springboot/project6/SpringBootProject6ApplicationTests.java
@@ -1,12 +1,25 @@
 package edu.buet.cse.springboot.project6;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import edu.buet.cse.springboot.project6.entity.Course;
+import edu.buet.cse.springboot.project6.jpa.CourseRepository;
 
 @SpringBootTest
 class SpringBootProject6ApplicationTests {
 
-  @Test
-  void contextLoads() {}
+  @Autowired
+  private CourseRepository courseRepository;
 
+  @Test
+  void testFindById() {
+    Course course = courseRepository.findById(1L);
+    assertTrue(course != null, "course is null");
+    assertEquals("Macro-Economics", course.getName(), "Course name is different than expected");
+  }
 }
