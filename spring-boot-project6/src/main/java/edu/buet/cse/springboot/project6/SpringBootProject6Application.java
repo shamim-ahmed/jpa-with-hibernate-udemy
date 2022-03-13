d48f9e73955cc8526b0d9f0ca99951a32231a884
Shamim Ahmed <shamim.buet.99@gmail.com>
2022-03-13 12:57:59 +1100

    Add logic to insert a new course in database

--- spring-boot-project6/src/main/java/edu/buet/cse/springboot/project6/SpringBootProject6Application.java
+++ spring-boot-project6/src/main/java/edu/buet/cse/springboot/project6/SpringBootProject6Application.java
@@ -25,7 +25,15 @@ public class SpringBootProject6Application implements ApplicationRunner {
 
   @Override
   public void run(ApplicationArguments args) throws Exception {
+    // find course by id
     Course course = courseRepository.findById(1L);
     logger.info("Course with id 1 -> {}", course);
+
+    // add a new course
+    Course discreteMath = new Course();
+    discreteMath.setName("Discrete Mathematics");
+    courseRepository.save(discreteMath);
+
+    logger.info("New course -> {}", discreteMath);
   }
 }
